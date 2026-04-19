e950c1984b3dc95cd19bad1f408685c776b83a1a
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2026-04-19 13:52:08 +0200

    Fix formatting issues in send_newsletter function

--- solutions/elixir/newsletter/3/lib/newsletter.ex
+++ solutions/elixir/newsletter/3/lib/newsletter.ex
@@ -19,8 +19,8 @@ defmodule Newsletter do
     log_file = open_log(log_path)
 
     emails_path
-    |> read_emails()
-    |> Enum.each(fn email -> if send_fun.(email) == :ok, do: log_sent_email(log_file, email) end)
+      |> read_emails()
+      |> Enum.each(fn email -> if send_fun.(email) == :ok, do: log_sent_email(log_file, email) end)
     
     close_log(log_file)
   end
